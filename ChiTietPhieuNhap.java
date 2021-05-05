b5116dbf66d230b81167d45047c84ae2b047a397
NGUYỄN VĂN TÚ <65603947+VTJVANTU@users.noreply.github.com>
2021-05-05 18:20:42 +0700

    Update ChiTietPhieuNhap.java

--- ChiTietPhieuNhap.java
+++ ChiTietPhieuNhap.java
@@ -1,10 +1,28 @@
-package kiemtra;
 
-public class ChiTietPhieuNhap {
-
-	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
+import java.util.Scanner;
+
+public class Chitietphieunhap extends PhieuNhap{
+    int MaPhieuNhap;
+    String TenPhieuNhap;
+    int SoLuong;
+    String HanSuDung;
+    
+    void nhap() {
+    	Scanner scanner = new Scanner(System.in);
+         System.out.print("Nhập mã phiếu: ");
+    MaPhieuNhap = scanner.nextInt();
+    System.out.print("Nhập tên phiếu nhập: ");
+    TenPhieuNhap = scanner.nextLine();
+    System.out.print("Nhập số lượng: ");
+    SoLuong = scanner.nextInt();
+    System.out.print("Nhập hạn sử dụng: ");
+    HanSuDung = scanner.nextLine();
+    }
+    void in(){
+         System.out.println("Mã phiếu:"+MaPhieuNhap +"\n" +"Tên Phiếu Nhâp:"+ TenPhieuNhap +"\n"+"Số lượng:"+ SoLuong +"hạn sử dụng: "+HanSuDung);
+    }
 }
